c4cb698b0723e962bc12feb41057afbbb28aef1d
metinen <maria.ilvonen@gmail.com>
2020-10-22 23:28:52 +0300

    Add 4-18

--- osa4/osa4/blogapp/app.js
+++ osa4/osa4/blogapp/app.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const logger = require('./utils/logger')
 
 logger.info('connecting to', config.MONGODB_URI)
@@ -21,6 +22,7 @@ app.use(cors())
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 module.exports = app
 

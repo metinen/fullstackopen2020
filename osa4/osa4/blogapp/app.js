0b2315e7561b1c5e0a76ef4864ed98392432cd48
metinen <maria.ilvonen@gmail.com>
2020-10-23 00:09:37 +0300

    Add 4-20

--- osa4/osa4/blogapp/app.js
+++ osa4/osa4/blogapp/app.js
@@ -7,6 +7,7 @@ const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
 const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
 
 logger.info('connecting to', config.MONGODB_URI)
 
@@ -20,6 +21,7 @@ mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology
 
 app.use(cors())
 app.use(express.json())
+app.use(middleware.tokenExtractor)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)

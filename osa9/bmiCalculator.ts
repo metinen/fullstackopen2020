d7e6b1f70fff0010ec2584ea380a50c9bc4924ed
metinen <maria.ilvonen@gmail.com>
2020-10-31 14:24:21 +0200

    Add 9-1

---
+++ osa9/bmiCalculator.ts
@@ -0,0 +1,17 @@
+const calculateBmi = (height: number, weight: number, ) => {
+    if (weight === 0) {
+        throw new Error("Weight should be over 0");
+    }
+    const bmi = weight / ((height * 0.01) ^ 2);
+
+    switch (true) {
+        case (bmi < 19):
+            return "Underweight"
+        case (bmi > 25):
+            return "Overweight"
+        default:
+            return "Normal (healthy weight)"
+    }
+}
+
+console.log(calculateBmi(180, 74));
